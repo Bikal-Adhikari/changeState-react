e4e0e53b3db1e33ef78f1f7e550263d58a09df8a
Bikal <bikaladhikari5@gmail.com>
2024-03-30 10:48:15 +1100

    Es6 spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,19 @@ function App() {
   const [contact, setContact] = useState({
     fName: "",
     lName: "",
-    email: ""
+    email: "",
   });
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
 
   return (
     <div className="container">
@@ -14,9 +25,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
@@ -24,3 +50,13 @@ function App() {
 }
 
 export default App;
+
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
+
+//HINT: You'll need to apply the following things you learnt:
+//1. Using JS Objects with state.
+//2. Making use of previous state when changing state.
+//3. Working with forms in React.
+//4. Handing events
